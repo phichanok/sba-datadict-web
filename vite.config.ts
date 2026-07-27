aab2cd48d5410506030e40cf5d0447c35a550453
phichanok <phichanok.ch@gmail.com>
2026-07-27 03:34:29 +0000

    use Node 22 for Vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,2 +1,6 @@
-declare const _default: import("vite").UserConfig;
-export default _default;
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+})
